a633487128b03f28a3a7dbb548f0907859d57f05
KeeganGrajek <Keegangrajek@gmail.com>
2022-09-20 22:02:54 -0700

    I think my computer is breaking
    I'm going to upload all changes to github quickly

--- test-website-3/index.js
+++ test-website-3/index.js
@@ -3,24 +3,26 @@ document.addEventListener('DOMContentLoaded', function(){
     var p = document.getElementsByClassName("foot-div");
     var initial = 1000;
     l[0].style.setProperty('display', 'flex');
-    l[0].style.setProperty('height', '100px');
-    setTimeout(function(){
-        l[0].style.setProperty('height', '400px');
-    }, 0);
-    setTimeout(function(){
-        l[0].style.setProperty('height', '100px');
-    }, initial);
-    setTimeout(function(){
-        p[0].style.setProperty('display', 'flex');
-        p[0].style.setProperty('height', '0px');
-    }, 0);
-    setTimeout(function(){
-        p[0].style.setProperty('height', '400px');
-    }, initial);
-    setTimeout(function(){
-        p[0].style.setProperty('height', '0px');
-    }, 1000+initial);
-    setTimeout(function(){
-        p[0].style.setProperty('display', 'none');
-    }, 2000+initial);
+    l[0].style.setProperty('height', '60px');
+    p[0].style.setProperty('display', 'flex');
+    p[0].style.setProperty('height', '20px');
+    // setTimeout(function(){
+    //     l[0].style.setProperty('height', '400px');
+    // }, 0);
+    // setTimeout(function(){
+    //     l[0].style.setProperty('height', '60px');
+    // }, initial);
+    // setTimeout(function(){
+    //     p[0].style.setProperty('display', 'flex');
+    //     p[0].style.setProperty('height', '0px');
+    // }, 0);
+    // setTimeout(function(){
+    //     p[0].style.setProperty('height', '400px');
+    // }, initial);
+    // setTimeout(function(){
+    //     p[0].style.setProperty('height', '0px');
+    // }, 1000+initial);
+    // setTimeout(function(){
+    //     p[0].style.setProperty('display', 'none');
+    // }, 2000+initial);
 });

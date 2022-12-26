c1b4e905cbe16651ae5807e681e9a5facdef1e2e
KeeganGrajek <Keegangrajek@gmail.com>
2022-12-26 14:18:14 -0800

    yesir

---
+++ test-website-5/index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var l = document.getElementsByClassName("head-div");
+    var p = document.getElementsByClassName("foot-div");
+    var initial = 1000;
+    l[0].style.setProperty('display', 'flex');
+    l[0].style.setProperty('height', '60px');
+    p[0].style.setProperty('display', 'flex');
+    p[0].style.setProperty('height', '20px');
+    // setTimeout(function(){
+    //     l[0].style.setProperty('height', '400px');
+    // }, 0);
+    // setTimeout(function(){
+    //     l[0].style.setProperty('height', '60px');
+    // }, initial);
+    // setTimeout(function(){
+    //     p[0].style.setProperty('display', 'flex');
+    //     p[0].style.setProperty('height', '0px');
+    // }, 0);
+    // setTimeout(function(){
+    //     p[0].style.setProperty('height', '400px');
+    // }, initial);
+    // setTimeout(function(){
+    //     p[0].style.setProperty('height', '0px');
+    // }, 1000+initial);
+    // setTimeout(function(){
+    //     p[0].style.setProperty('display', 'none');
+    // }, 2000+initial);
+});
